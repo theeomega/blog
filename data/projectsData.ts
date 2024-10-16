948a0da894283f8a7771f8509703be6f0b5f97a8
Muhammad Awais <147119663+Aw4iskh4n@users.noreply.github.com>
2024-10-16 12:23:51 +0500

    Update projectsData.ts

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -8,13 +8,13 @@ interface Project {
 const projectsData: Project[] = [
   {
     title: 'File Organizer',
-    description: `The File Organizer Tool swiftly sorts files in directories by their extensions. With a user-friendly interface, it categorizes files into groups like 'Audio' or 'Video'. Its standout 'Undo' feature allows easy reversals, ensuring flexibility and ease for users. Perfect for decluttering digital spaces efficiently.`,
+    description: "The File Organizer Tool swiftly sorts files in directories by their extensions. With a user-friendly interface, it categorizes files into groups like 'Audio' or 'Video'. Its standout 'Undo' feature allows easy reversals, ensuring flexibility and ease for users. Perfect for decluttering digital spaces efficiently.",
     imgSrc: '/static/images/google.png',
     href: 'https://github.com/Aw4iskh4n/FolderOrganizer-GUI',
   },
   {
     title: 'PwnedPasswd',
-    description: `It is a secure tool that allows users to check if their passwords have been exposed in known data breaches. Harnessing the power of Python 🐍 and Flask, it ensures that user passwords are not sent or stored, offering peace of mind about online security.`,
+    description: "It is a secure tool that allows users to check if their passwords have been exposed in known data breaches. Harnessing the power of Python 🐍 and Flask, it ensures that user passwords are not sent or stored, offering peace of mind about online security.",
     imgSrc: '/static/images/time-machine.jpg',
     href: 'https://github.com/Aw4iskh4n/pwnedpasswd',
   },
