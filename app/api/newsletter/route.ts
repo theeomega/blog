8fa48c1070dec1bdea97427c77eec7d2f397eeb7
Muhammad Awais <147119663+Aw4iskh4n@users.noreply.github.com>
2025-03-04 06:22:22 +0500

    Update route.ts

--- app/api/newsletter/route.ts
+++ app/api/newsletter/route.ts
@@ -2,8 +2,14 @@ import { NewsletterAPI } from 'pliny/newsletter'
 import siteMetadata from '@/data/siteMetadata'
 
 const handler = NewsletterAPI({
+
+  providers: {
+    buttondown: {
+      apiKey: process.env.BUTTONDOWN_API_KEY
+    }
+  }
   // @ts-ignore
-  provider: siteMetadata.newsletter.provider,
+  // provider: siteMetadata.newsletter.provider,
 })
 
 export { handler as GET, handler as POST }
