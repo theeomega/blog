21876c43d6ef3cce34d2050e953861dff351edbb
Muhammad Awais <147119663+Aw4iskh4n@users.noreply.github.com>
2025-03-04 06:25:27 +0500

    Update route.ts

--- app/api/newsletter/route.ts
+++ app/api/newsletter/route.ts
@@ -3,13 +3,13 @@ import siteMetadata from '@/data/siteMetadata'
 
 const handler = NewsletterAPI({
 
-  providers: {
-    buttondown: {
-      apiKey: process.env.BUTTONDOWN_API_KEY
-    }
-  }
+  // providers: {
+  //   buttondown: {
+  //     apiKey: process.env.BUTTONDOWN_API_KEY
+  //   }
+  // }
   // @ts-ignore
-  // provider: siteMetadata.newsletter.provider,
+  provider: siteMetadata.newsletter.provider,
 })
 
 export { handler as GET, handler as POST }
