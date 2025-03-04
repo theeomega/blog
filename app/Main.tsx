fe3f4538465fcc119c5903bdd7f89f0591d19c4a
Muhammad Awais <147119663+Aw4iskh4n@users.noreply.github.com>
2025-03-04 16:13:08 +0500

    Update Main.tsx

--- app/Main.tsx
+++ app/Main.tsx
@@ -2,8 +2,8 @@ import Link from '@/components/Link'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
-import NewsletterForm from '@/components/pliny/ui/NewsletterForm'
-// import NewsletterForm from 'pliny/ui/NewsletterForm'
+// import NewsletterForm from '@/components/pliny/ui/NewsletterForm'
+import NewsletterForm from 'pliny/ui/NewsletterForm'
 
 const MAX_DISPLAY = 5
 
